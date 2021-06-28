98020ff32203b02a314a5e053b8eb0f906328727
officialputuid <officialputuid@hack.id>
2021-06-28 18:42:39 +0800

    toolsig: Cleanup, after this will update new version
    Signed-off-by: officialputuid <officialputuid@hack.id>

--- index.js
+++ index.js
@@ -8,69 +8,68 @@ var os = require("os");
 var interfaces = os.networkInterfaces();
 var addresses = [];
 for (var k in interfaces) {
-  for (var k2 in interfaces[k]) {
-    var address = interfaces[k][k2];
-    if (address.family === "IPv4" && !address.internal) {
-      addresses.push(address.address);
+    for (var k2 in interfaces[k]) {
+        var address = interfaces[k][k2];
+        if (address.family === "IPv4" && !address.internal) {
+            addresses.push(address.address);
+        }
     }
-  }
 }
 //DETECT IP *END!
 const questionTools = [
-  "➥ Information",
-  "➥ Bot Like Timeline",
-  "➥ Bot Like Target User",
-  "➥ Mass Delete Post/Photo",
+    "➥ Information",
+    "➥ Bot Like Timeline",
+    "➥ Bot Like Target User",
+    "➥ Mass Delete Post/Photo",
 
-  "➥ F-L -> Followers Target",
-  "➥ L-C -> Followers Target",
+    "➥ F-L -> Followers Target",
+    "➥ L-C -> Followers Target",
 
-  "➥ F-L-C -> Followers Target",
-  "➥ F-L-C -> Followers Target [BETA]",
+    "➥ F-L-C -> Followers Target",
+    "➥ F-L-C -> Followers Target [BETA]",
 
-  "➥ F-L-C -> Followers Target v2",
+    "➥ F-L-C -> Followers Target v2",
 
-  "➥ F-L-DM -> Followers Target",
-  "➥ F-L-DM -> Followers Target [BETA]",
+    "➥ F-L-DM -> Followers Target",
+    "➥ F-L-DM -> Followers Target [BETA]",
 
-  "➥ F-L-C -> Hashtag Target",
-  "➥ F-L-C -> Location Target",
+    "➥ F-L-C -> Hashtag Target",
+    "➥ F-L-C -> Location Target",
 
-  "➥ Unfollow All Following",
-  "➥ Unfollow Not Followback",
-  "\n",
+    "➥ Unfollow All Following",
+    "➥ Unfollow Not Followback",
+    "\n",
 ];
 
 const menuQuestion = {
-  type: "list",
-  name: "choice",
-  message:
-    "Select tools:\n  Read the (❆ Information) first before using the tool! Nyaa~\n\n",
-  choices: questionTools,
+    type: "list",
+    name: "choice",
+    message: "Select tools:\n  Read the (❆ Information) first before using the tool! Nyaa~\n\n",
+    choices: questionTools,
 };
 
 const main = async () => {
-  try {
-    const { choice } = await inquirer.prompt(menuQuestion);
-    choice == questionTools[0] && require("./tools/info.js");
-    choice == questionTools[1] && require("./tools/liketimeline.js");
-    choice == questionTools[2] && require("./tools/liketarget.js");
-    choice == questionTools[3] && require("./tools/delallmedia.js");
-    choice == questionTools[4] && require("./tools/flonly.js");
-    choice == questionTools[5] && require("./tools/lconly.js");
-    choice == questionTools[6] && require("./tools/fftauto.js");
-    choice == questionTools[7] && require("./tools/fftbetaauto.js");
-    choice == questionTools[8] && require("./tools/fftautov2.js");
-    choice == questionTools[9] && require("./tools/fftdmauto.js");
-    choice == questionTools[10] && require("./tools/fftdmbetaauto.js");
-    choice == questionTools[11] && require("./tools/fhtauto.js");
-    choice == questionTools[12] && require("./tools/fltauto.js");
-    choice == questionTools[13] && require("./tools/unfollowall.js");
-    choice == questionTools[14] && require("./tools/unfollnotfollback.js");
-    choice == questionTools[15] && process.exit();
-  } catch (err) {
-    print(err, "err");
-  }
+    try {
+        const { choice } = await inquirer.prompt(menuQuestion);
+        choice == questionTools[0] && require("./tools/info.js");
+        choice == questionTools[1] && require("./tools/liketimeline.js");
+        choice == questionTools[2] && require("./tools/liketarget.js");
+        choice == questionTools[3] && require("./tools/delallmedia.js");
+        choice == questionTools[4] && require("./tools/flonly.js");
+        choice == questionTools[5] && require("./tools/lconly.js");
+        choice == questionTools[6] && require("./tools/fftauto.js");
+        choice == questionTools[7] && require("./tools/fftbetaauto.js");
+        choice == questionTools[8] && require("./tools/fftautov2.js");
+        choice == questionTools[9] && require("./tools/fftdmauto.js");
+        choice == questionTools[10] && require("./tools/fftdmbetaauto.js");
+        choice == questionTools[11] && require("./tools/fhtauto.js");
+        choice == questionTools[12] && require("./tools/fltauto.js");
+        choice == questionTools[13] && require("./tools/unfollowall.js");
+        choice == questionTools[14] && require("./tools/unfollnotfollback.js");
+        choice == questionTools[15] && process.exit();
+    } catch (err) {
+        print(err, "err");
+    }
 };
 
 console.log(chalk`{bold.green
@@ -87,9 +86,7 @@ console.log(chalk`{bold.green
   116 111 111 108 115 105 103  118 51 
   }`);
 console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
-console.log(
-  "  Ξ START  : ".bold.red + moment().format("D MMMM YYYY, h:mm:ss a")
-);
+console.log("  Ξ START  : ".bold.red + moment().format("D MMMM YYYY, h:mm:ss a"));
 console.log("  Ξ YPATH  : ".bold.red + userHome);
 console.log("  Ξ YOUIP  : ".bold.red + addresses);
 console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
